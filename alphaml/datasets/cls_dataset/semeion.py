049736089e532cca0360af06469a3ab8c33b7540
Thomas Young <thomasyoung@ThomasdeMacBook-Pro.local>
2019-06-19 01:43:37 +0800

    update ts

--- alphaml/datasets/cls_dataset/semeion.py
+++ alphaml/datasets/cls_dataset/semeion.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def load_semeion(data_folder):
-    file_path = data_folder + 'phpfLuQE4.csv'
+    file_path = data_folder + 'phptd5jYj.csv'
     data = pd.read_csv(file_path, delimiter=',').values
     labels = data[:, -1] - 1
     features = data[:, :-1]
