d42b32f9070d07199481d0f4b1d97dfed8df570e
thomas-young-2013 <1225646303@qq.com>
2019-08-09 16:02:39 +0800

    update

--- alphaml/engine/components/pipeline/data_preprocessing_pipeline.py
+++ alphaml/engine/components/pipeline/data_preprocessing_pipeline.py
@@ -2,7 +2,9 @@ import abc
 import typing
 from pandas import DataFrame
 from alphaml.engine.components.data_manager import DataManager
-
+from alphaml.engine.components.pipeline.data_preprocessing_operator import *
+from alphaml.engine.components.pipeline.feature_generation_operator import *
+from alphaml.engine.components.pipeline.feature_selection_operator import *
 
 """
 Pipeline Framework:
@@ -14,11 +16,36 @@ Pipeline Framework:
 class DP_Pipeline(object):
     def __init__(self, pipeline_config):
         """Reconstruct the data preprocessing pipeline according to the config."""
+        # Create the default DP graph.
+        # 1. Create the basic nodes.
         self.pipeline_operators = list()
+        node1 = ImputerOperator()
+        node5 = FeatureEncoderOperator()
+        node2 = NormalizerOperator()
+        node3 = ScalerOperator()
+        node4 = LabelEncoder()
+
+        self.pipeline_operators.extend([node1, node2, node3, node4, node5])
+        self.cached_dm = dict()
+
+        # Assign the node id.
+        for node_id, node in enumerate(self.pipeline_operators):
+            node.id = node_id
+            if node_id != 0:
+                node.origin = [node_id - 1]
 
     def execute(self, input: DataFrame) -> DataManager:
-        input_data = [input]
+        # DM caches.
+        for node_id in range(len(self.pipeline_operators)):
+            self.cached_dm[node_id] = None
+
         for operator in self.pipeline_operators:
-            # Prepare the input the operator.
-            input_data = operator.operate(input_data)
-        return input_data
+            # Prepare the input dm for the operator.
+            if operator.id == 0:
+                input_dm = [input]
+            else:
+                input_dm = [self.cached_dm[id] for id in operator.origin]
+            output_dm = operator.operate(input_dm)
+            self.cached_dm[operator.id] = output_dm
+
+        return self.cached_dm[len(self.pipeline_operators)-1]
