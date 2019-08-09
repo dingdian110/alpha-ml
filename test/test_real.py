9f21d141c4bc455333f3b6315973ff73f698178c
salty-fish-97 <459240868@qq.com>
2019-08-09 16:06:16 +0800

    Create test_real.py

---
+++ test/test_real.py
@@ -0,0 +1,41 @@
+from alphaml.engine.components.data_manager import DataManager
+from alphaml.estimators.classifier import Classifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+import pandas as pd
+from alphaml.engine.components.pipeline.data_preprocessing_operator import ImputerOperator, ScalerOperator
+import sys
+
+sys.path.append('/home/daim_gpu/sy/AlphaML')
+
+
+def test_cash_module():
+    data = pd.read_csv("data/cls_data/santander/train.csv")
+    dm = ImputerOperator().operate([data.drop(columns=["ID"])])
+    dm.split(stratify=True)
+    x_test = dm.val_X
+    y_test = dm.val_y
+    dm.split(stratify=True)
+    cls = Classifier(include_models=['xgboost', 'random_forest', 'libsvm_svc', 'decision_tree'],
+                     optimizer='mono_smbo',
+                     # ensemble_method='stacking',
+                     # ensemble_size=6,
+                     ).fit(dm, metric='auc', runcount=250)
+    data = pd.read_csv("data/cls_data/santander/test.csv").values
+    x_data = data[:, 1:]
+    pred1 = cls.predict(x_test)
+    pred2 = cls.predict(x_data)
+    print('Valid Out:', pred1)
+    print("Valid: Best", roc_auc_score(y_test, pred1))
+
+    import csv
+    with open('data/cls_data/santander/submission.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['ID', 'TARGET'])
+        for i in range(len(pred2)):
+            line = [int(data[i, 0]), pred2[i]]
+            writer.writerow(line)
+
+
+if __name__ == "__main__":
+    test_cash_module()
