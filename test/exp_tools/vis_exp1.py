148b377b65ba9ec27b0184cbe6a01a5712896527
thomas-young-2013 <1225646303@qq.com>
2019-08-22 01:09:29 +0800

    update

--- test/exp_tools/vis_exp1.py
+++ test/exp_tools/vis_exp1.py
@@ -6,6 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--mode', choices=['master', 'daim213', 'gc'], default='gc')
 parser.add_argument('--rep', type=int, default=10)
 parser.add_argument('--dataset', type=str, default='pc4')
+parser.add_argument('--start_runid', type=int, default=0)
 args = parser.parse_args()
 
 if args.mode == 'master':
@@ -18,13 +19,18 @@ else:
     raise ValueError('Invalid mode: %s' % args.mode)
 
 
-def plot(dataset, rep_num):
+def plot(dataset, rep_num, start_id):
     task_id = 'exp_1_evaluation_500'
     mth_list = ['avg_smac', 'cmab_ts_smac', 'smac',
-                'mm_bandit_3_smac', 'softmax_mab_1_1.0000_smac', 'ucb_mab_1_0.0000_smac']
-    for mth in mth_list:
+                'softmax_mab_1_1.0000_smac', 'ucb_mab_1_0.0000_smac', 'mm_bandit_3_smac']
+    optimizer_algos = ['baseline_2', 'cmab_ts', 'smbo', 'rl_2_1', 'rl_3_0', 'mono_smbo_3_0']
+    assert len(mth_list) == len(optimizer_algos)
+    exp_result = dict()
+    for idx, mth in enumerate(mth_list):
+        exp_result[mth] = list()
         perfs = list()
         tmp_d = dataset.split('_')[0]
+        # Extract validation performance.
         for id in range(rep_num):
             file_id = project_folder + '/data/%s/%s_%s_%d_%s.data' % (tmp_d, dataset, task_id, id, mth)
             print(file_id)
@@ -33,8 +39,21 @@ def plot(dataset, rep_num):
             perf_t = np.max(data['perfs'])
             print(mth, data['time_cost'][-1], perf_t)
             perfs.append(perf_t)
-        print('='*10, mth, np.mean(perfs))
+        assert len(perfs) == rep_num
+        mean_perf = np.mean(perfs)
+        print('='*10, mth, mean_perf)
+        exp_result[mth].append(mean_perf)
+
+        # Extract test performance.
+        with open('data/%s/%s_test_result_%s_%s_%d_%d.pkl' %
+                  (tmp_d, dataset, optimizer_algos[idx], task_id, rep_num, start_id), 'wb') as f:
+            test_data = pickle.load(f)
+        assert len(test_data.values()) == rep_num
+        exp_result[mth].append(np.mean(test_data.values()))
+    print('='*50)
+    print(exp_result)
+    print('='*50)
 
 
 if __name__ == "__main__":
-    plot(args.dataset, args.rep)
+    plot(args.dataset, args.rep, args.start_runid)
